1d279ec46afb7f251887a1c1f2213837c2e32eef
vibgupta06 <162367861+vibgupta06@users.noreply.github.com>
2024-03-07 14:29:55 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,32 +1,57 @@
+
 variable "location" {
-    type = string
-    default = "eastus"
+  type    = string
+  default = "eastus"
 }
 
 variable "address_space" {
-    type = string
-    default = "10.0.0.0/16"
+  type    = string
+  default = "10.0.0.0/16"
 }
 
 variable "address_prefixes" {
-    type = string
-    default = "10.0.0.0/24"
+  type    = string
+  default = "10.0.0.0/24"
 }
 
 variable "size" {
-    type = string
-    default = "Standard_B1s"
+  type    = string
+  default = "Standard_B1s"
 }
 
 variable "storage_account_type" {
-    type = string
-    default = "Standard_LRS"
+  type    = string
+  default = "Standard_LRS"
 }
 
 variable "admin_username" {
-    type = string
+  type = string
+  default = "user.admin"
+  sensitive = true
 }
 
 variable "admin_password" {
-    type = string
+  type = string
+  sensitive = true
+}
+
+variable "vm_map" {
+    type = map(object({
+      name = string
+      size = string
+      
+    }))
+    default = {
+      "vm1" = {
+        name = "tf-vm1-eastus"
+        size = "Standard_B1s"
+        
+      }
+      "vm2" = {
+        name = "tf-vm2-eastus"
+        size = "Standard_B2s"
+        
+      }
+    }
+  
 }
